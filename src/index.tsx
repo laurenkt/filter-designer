f98f36c8d8f4ceca1be765b6ee26ef5e746e4343
Lauren <lauren@tomasello.me>
2018-09-21 12:08:15 +0200

    Refactor feature detection

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,33 @@
 import React from "react"
 import { render } from "react-dom"
-import FeatureDetector from "./components/FeatureDetector"
+import FeatureDetector, { Feature } from "./components/FeatureDetector"
 import App from "./App"
 import "./style.scss"
 
+const FeatureFallback = (features: Feature[]) => (
+    <>
+        <p style={{ maxWidth: 500, textAlign: "center" }}>
+            This is a tech demo and uses features that are only recently
+            developed and don't currently have widespread adoption. Your browser
+            does not support some of the necessary features. See below for which
+            features are required and which browsers you can use to get them.
+        </p>
+        <table>
+            <tbody>
+                {features.map(({ friendlyName, available }) => (
+                    <tr key={friendlyName}>
+                        <th>{friendlyName}</th>
+                        <td>{available ? "✅" : "❌"}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </>
+)
+
 document.addEventListener("DOMContentLoaded", _ => {
     render(
-        <FeatureDetector
-            fallback={features => (
-                <>
-                    <p style={{ maxWidth: 500, textAlign: "center" }}>
-                        This is a tech demo and uses features that are only
-                        recently developed and don't currently have widespread
-                        adoption. Your browser does not support some of the
-                        necessary features. See below for which features are
-                        required and which browsers you can use to get them.
-                    </p>
-                    <table>
-                        <tbody>
-                            {features.map(({ friendlyName, available }) => (
-                                <tr key={friendlyName}>
-                                    <th>{friendlyName}</th>
-                                    <td>{available ? "✅" : "❌"}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </>
-            )}
-        >
+        <FeatureDetector fallback={FeatureFallback}>
             <App />
         </FeatureDetector>,
         document.querySelector("main")
